b66e854923b82a86277555117efb28edda68b0a6
Chiranjeevi <cchimmili3@gatech.edu>
2022-08-11 08:54:35 -0400

    init

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import './Contact.css'
+
+const Contact = () => {
+
+  return (
+    <section className='section contact center' id='contact'>
+      <h2 className='section__title'>Contact</h2>
+      <a href="mailto:cchimmili3@gatech.edu">
+        <span type='button' className='btn btn--outline'>
+          Email me
+        </span>
+      </a>
+    </section>
+  )
+}
+
+export default Contact
